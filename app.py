80fec561623fc6df2b21ad877798deef8af9fc94
trevorzhen <trevorzhen@gmail.com>
2021-08-09 09:29:21 +0200

    Add files via upload
    updated webpage

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ def predict(model,input_df):
     return predictions
 
 def run():
-
-    st.title("Insurance Claim Prediction")
+    st.set_page_config(page_title= 'Insurance', page_icon= 'random', layout='centered', initial_sidebar_state='auto')
 
     from PIL import Image
     image = Image.open('medical.jpg')
@@ -19,12 +18,26 @@ def run():
 
     st.image(image)
 
-    add_selectbox = st.sidebar.selectbox("How would you like to predict?", ("Online","Batch"))
+    st.title("Insurance Claim Prediction")
+
+    add_selectbox = st.sidebar.selectbox("How would you like to predict?", ("Stress Test","Online","Batch"))
 
     st.sidebar.info("This app predicts patient medical expenses using Gradient Boosting Regressor.")
-    st.sidebar.success("http://trevorzhen.com/")
+    st.sidebar.success("https://www2.deloitte.com/ch/en.html")
 
     st.sidebar.image(image_medical)
+
+    if add_selectbox == "Stress Test":
+        st.header("Please provide Patient ID (from 0 to 1337):")
+        patient_id = st.number_input("Patient ID", min_value= 0 , max_value = 1337, value = 2)
+
+        data = pd.read_csv('insurance.csv')
+        prediction = predict_model(estimator=model, data = data.iloc[[patient_id]])
+        prediction = prediction.rename(columns={"Label":'predictions'})
+        output1 = '$' + str(round(prediction.iloc[0][7],2))
+        output2 = '$' + str(round(prediction.iloc[0][6],2))
+        st.success('The predicted amount of the claim of Patient No.{} is {}, while his/her actual claim is {}.'.format(patient_id, output1, output2))
+        st.write(prediction.style.set_precision(2))
 
     if add_selectbox == "Online":
 
@@ -53,8 +66,7 @@ def run():
         if st.button("Predict"):
             output = predict(model=model, input_df= input_df)
             output = '$' + str(round(output,2))
-        
-        st.success('The expected amount of the claim is {}.'.format(output))
+            st.success('The predicted amount of the claim is {}.'.format(output))
 
     if add_selectbox == "Batch":
         file_upload = st.file_uploader("Please upload the .csv file for predictions", type = ["csv"])
@@ -62,7 +74,8 @@ def run():
         if file_upload is not None:
             data = pd.read_csv(file_upload)
             predictions = predict_model(estimator=model, data = data)
-            st.write(predictions)
+            predictions = predictions.rename(columns={"Label":'predictions'})
+            st.write(predictions.style.set_precision(2))
 
 if __name__ == '__main__':
     run()
